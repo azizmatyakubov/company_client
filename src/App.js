faa58251dd4de3855680c870cefb58404a9fd12a
AZIZ <a.matyaqubov1471@gmail.com>
2022-11-30 19:30:08 +0100

    Update app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route, Navigate } from "react-router-dom";
+import MainLayout from './layouts/MainLayout';
+import ConsoleLayout from './layouts/ConsoleLayout';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Logout from './pages/Logout';
+import Signup from './pages/Signup';
+import ForgotPassword from './pages/ForgotPassword';
+import NotFound from './pages/NotFound';
+import Profile from './pages/Profile';
+import Dashboard from './pages/Dashboard';
+import Apps from './pages/Apps';
+import Users from './pages/Users';
+import Settings from './pages/Settings';
 
-function App() {
+function  App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <Routes>
+          <Route path="/" element={<MainLayout />}>
+            <Route path="" element={<Home />} />
+            <Route path="login" element={<Login />} />
+            <Route path="logout" element={<Logout />} />
+            <Route path="signup" element={<Signup />} />
+            <Route path="forgot" element={<ForgotPassword />} />
+            <Route path="not-found" element={<NotFound />} />
+            <Route path="*" element={<Navigate to="/not-found" />} />
+        
+          </Route>
+          {/* Protected Routes */}
+          <Route element={<ConsoleLayout />}>
+            <Route path="dashboard" element={<Dashboard />} />
+            <Route path="profile" element={<Profile />} />
+            <Route path="apps" element={<Apps />} />
+            <Route path="users" element={<Users />} />
+            <Route path="settings" element={<Settings />} />
+          </Route>
+        </Routes>
+
   );
 }
 
---
+++ src/pages/Login.js
@@ -0,0 +1,110 @@
+import { useRef, useState, useEffect } from 'react';
+import { Helmet } from 'react-helmet';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import {
+  Button, Col, Form, FormControl, FormLabel, Row, Spinner,
+} from 'react-bootstrap';
+import useAuth from '../hooks/useAuth';
+
+
+import './auth.css';
+
+
+function Login() {
+  const title = 'Login';
+  const userRef = useRef();
+  const errRef = useRef();
+  const [email, setEmail] = useState('');
+  const [pass, setPass] = useState('');
+  const [errMsg, setErrMsg] = useState('');
+  const navigate = useNavigate();
+  const { setAuth } = useAuth(); 
+
+ 
+
+  useEffect(() => {
+    userRef.current.focus();
+  }, []);
+
+  useEffect(() => {
+    setErrMsg('')
+  }, [email, pass]);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const res = await fetch('/api/v1/auth/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          email: email,
+          password: pass,
+        }),
+        credentials: 'include',
+        // prevent CORS errors.
+        mode: 'cors',
+      });
+
+      if (res.ok) {
+        const { accessToken } = await res.json();
+        setAuth(accessToken);
+        navigate('/dashboard');
+      } 
+
+    } catch (err) {
+      setErrMsg(err);
+    }
+
+  };
+
+  const handleUserChange = (e) => setEmail(e.target.value);
+  const handlePassChange = (e) => setPass(e.target.value);
+
+
+  return (
+    <>
+      <Helmet>
+        <title>{title}</title>
+      </Helmet>
+      <main className="container-auth text-center">
+        <Form onSubmit={handleSubmit}>
+          <i className="bi bi-file-lock-fill auth-icon my-4"/>
+          <p ref={errRef}>Error</p>
+          <p className="mb-3 fw-normal">
+            Click <strong>Log in</strong> button to log into the admin console.
+            Use <strong>admin</strong>:<strong>qwerty</strong> to log in.
+          </p>
+          <Form.Group className="form-floating" controlId="inputUsername">
+            <FormControl type="email"
+                         value={email}
+                         className="form-control form-input-top"     
+                         ref={userRef}
+                         placeholder="Email"
+                         onChange={handleUserChange}
+                         required
+            />
+            <FormLabel>Username</FormLabel>
+          </Form.Group>
+          <Form.Group className="form-floating" controlId="inputPassword">
+            <FormControl type="password"
+                         value={pass}
+                         className="form-control form-input-top"     
+                         placeholder="Password"
+                         onChange={handlePassChange}
+                         required 
+            />
+            <FormLabel>Password</FormLabel>
+          </Form.Group>
+          <Button variant="primary" type="submit" className="w-100">
+            Log in
+          </Button>
+        </Form>
+      </main>
+    </>
+  );
+}
+
+export default Login;
